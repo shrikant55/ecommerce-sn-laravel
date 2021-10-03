eca2d049a013d579c99f315e9e6d25e5d9da451b
Shrikant <shrikantn55@gmail.com>
2021-10-03 14:12:44 +0530

    login page

---
+++ resources/views/master.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Master</title>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+        <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <!-- <h1>Master Page</h1> -->
+        {{ View::make('header')}}
+        @yield('content')
+        {{ View::make('footer')}}
+    </body>
+    <!-- <script>
+                $(document).ready(function()
+                {
+                    $("button").click(function()
+                    {
+                        alert("all set")
+                    })
+                    
+                })
+            </script> -->
+    </html>
